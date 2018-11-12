1a2c52d68c5cd9d06334de9f9fee48b78c052a32
porisika <132691510252163.com>
2018-11-12 20:26:55 +0800

    [ web]数据绑定

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,4 +2,24 @@
 //获取应用实例
 const app = getApp()
 
-Page({})
+Page({
+  data:{
+    weather:'阴天',
+    temp:'20"'
+  },
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '厦门市'
+      },
+      success: res => {
+        let xiamenList = res.data.result;
+        xiamenList.forecast.forEach(function(v){
+          console.log(v.weather);
+          console.log(v.temp);
+        });
+      }
+    })
+  },
+})
