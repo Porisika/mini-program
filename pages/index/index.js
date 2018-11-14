81380cb1cde5eb23dd4bed1ec45ad09fca0c8f6f
porisika <132691510252163.com>
2018-11-14 19:35:36 +0800

    [web]接API

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,9 +18,32 @@ const weatherColorMap = {
 
 Page({
   data:{
-    temp: '20"',
-    weather:'阴天'
+    temp: '',
+    weather:'',
+    forecastArr:[],
+    weatherIocn:{
+      'sunny': '☀️',
+      'cloudy': '☁️',
+      'overcast': '☁️',
+      'lightrain': '☔️',
+      'heavyrain': '⛈️',
+      'snow': '❄️',
+    },
+    weatherMap:{
+      'sunny': '晴天',
+      'cloudy': '多云',
+      'overcast': '阴天',
+      'lightrain': '小雨',
+      'heavyrain': '大雨',
+      'snow': '雪',
+    },
+    citys:['北京市','香港市','厦门市'],
+    // index:0,
+    // city:citys[index],
   },
+  // scrollX:()=>{
+    // this.city = citys[index++],
+  // },
   getNow(){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
@@ -29,11 +52,15 @@ Page({
       },
       success: res => {
         let result = res.data.result;
-        let temp = result.now.temp;
-        let weather = result.now.weather;
-        console.log(result);
-        console.log(temp);
-        console.log(weather);
+        let forecast = result.forecast;
+        let nowTemp = result.now.temp;
+        let nowWeather = result.now.weather;
+
+        this.setData({
+          temp: nowTemp,
+          weather: weatherMap[nowWeather],
+          forecastArr:forecast,
+        });
       }
     })
   },
