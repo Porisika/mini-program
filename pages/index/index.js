7bdd42406911f2f57c4aa6c828c590e75dae52b8
porisika <132691510252163.com>
2018-11-15 16:15:03 +0800

    [web]设置页面转发功能

--- pages/index/index.js
+++ pages/index/index.js
@@ -38,23 +38,20 @@ Page({
       'snow': '雪',
     },
     citys:['北京市','香港市','厦门市'],
-    // index:0,
-    // city:citys[index],
+    city:'香港市',
   },
-  // scrollX:()=>{
-    // this.city = citys[index++],
-  // },
   getNow(){
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city: '厦门市'
+        city: '北京市',
       },
       success: res => {
         let result = res.data.result;
         let forecast = result.forecast;
         let nowTemp = result.now.temp;
         let nowWeather = result.now.weather;
+        console.log(result);
 
         this.setData({
           temp: nowTemp,
@@ -67,7 +64,33 @@ Page({
   onPullDownRefresh(){
    this.getNow();
   },
+  shareMessage(){
+    wx.showShareMenu({
+      withShareTicket: false,
+      title: '蠡湖',
+      imageUrl: 'https://images.unsplash.com/photo-1498335746477-0c73d7353a07?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=1e8d56c5d16f5f0e6198f9920b9052dc&auto=format&fit=crop&w=700&q=60',
+    });
+  },
   onLoad(){
    this.getNow();
+    this.shareMessage();
+  },
+  scrollX(){
+    this.setData({
+      city:'厦门市'
+    });
+    this.getNow();
+  },
+  changeNavigationBarColor(){
+    wx.setNavigationBarTitle({
+      title: '蠡湖',
+    });
+    wx.setNavigationBarColor({
+      frontColor: '#ffffff',
+      backgroundColor: '#ffffff',
+    })
+  },
+  onPageScroll(){
+    console.log('滚动中');
   },
 })
