f437f1d3d4f721ac41bb723d03ae344ba30612a3
porisika <132691510252163.com>
2019-05-12 18:51:27 +0800

    调用网络数据、动态展示数据和下拉刷新

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,23 +1,127 @@
 // pages/index/index.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
+Page({
 
   /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
+    nowTemp:'',
+    nowWeather:'',
+    nowWeatherBackground:'',
+  },
+  getNow(callback){
+    console.log('小程序刚刚启动啦!');
+    var that = this;
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now', // 仅为示例，并非真实的接口地址
+      data: {
+        city: '上海市'
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        const weatherMap = {
+          'sunny': '晴天',
+          'cloudy': '多云',
+          'overcast': '阴',
+          'lightrain': '小雨',
+          'heavyrain': '大雨',
+          'snow': '雪'
+        }
+        //导航栏颜色
+        const weatherColorMap = {
+          'sunny': '#cbeefd',
+          'cloudy': '#deeef6',
+          'overcast': '#c6ced2',
+          'lightrain': '#bdd5e1',
+          'heavyrain': '#c5ccd0',
+          'snow': '#aae1fc'
+        }
+        var result = res.data.result;
+        var temp = result.now.temp;
+        var weather = result.now.weather;
+        console.log(result);
+        // 动态渲染当前天气状况(气温、天气描述、背景图片)
+        that.setData({
+          nowTemp: temp,
+          nowWeather: weatherMap[weather],
+          nowWeatherBackground: '/images/' + weather + '-bg.png'
+        });
+        //设置导航栏的颜色
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather]
+        });
 
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+  /**
+   * 下拉刷新重新发送请求加载数据
+   * */
+  onPullDownRefresh() {
+    this.getNow(()=>{
+    wx.stopPullDownRefresh()
+    });
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
+  onLoad: function (options) {
+    this.getNow();    
+  },
 
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
   }
 })
