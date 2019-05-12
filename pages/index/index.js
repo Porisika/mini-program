628bfecd3371696d3aba173e2fe9440b6a9a6be4
porisika <132691510252163.com>
2019-05-12 21:44:36 +0800

    获取未来24小时天气预报,添加了横向的滑动列表

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,6 +8,7 @@ Page({
     nowTemp:'',
     nowWeather:'',
     nowWeatherBackground:'',
+    forecast:[],
   },
   getNow(callback){
     console.log('小程序刚刚启动啦!');
@@ -38,9 +39,10 @@ Page({
           'heavyrain': '#c5ccd0',
           'snow': '#aae1fc'
         }
-        var result = res.data.result;
-        var temp = result.now.temp;
-        var weather = result.now.weather;
+        let result = res.data.result;
+        let temp = result.now.temp;
+        let weather = result.now.weather;
+        let forecastList = result.forecast;
         console.log(result);
         // 动态渲染当前天气状况(气温、天气描述、背景图片)
         that.setData({
@@ -53,7 +55,19 @@ Page({
           frontColor: '#000000',
           backgroundColor: weatherColorMap[weather]
         });
-
+        //获取天气预报
+        let nowHour = new Date().getHours();
+        let forecast =[];
+        for (let i = 0; i < forecastList.length;i++){
+          forecast.push({
+            time: i===0? '现在':(i + nowHour)%24+'时',
+            iconPath: '/images/' + forecastList[i].weather+'-icon.png',
+            temp: forecastList[i].temp + '°'
+          });
+          that.setData({
+            forecast:forecast
+          });
+        }
       },
       complete: () => {
         callback && callback()
